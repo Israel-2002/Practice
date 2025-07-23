d03ca5449b9e97757f743afc29025d828fb27994
izzy <israelrankin@gmail.com>
2025-07-23 17:27:09 +0000

    updated port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "src/views"));
